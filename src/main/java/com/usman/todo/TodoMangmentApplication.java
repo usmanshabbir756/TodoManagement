e98adee8f0ea4f4308bee60566b99c2faa92e588
usmanshabbir756 <usmanshabbir756@gmail.com>
2024-02-10 20:15:11 +0500

    adding modelmapper

--- src/main/java/com/usman/todo/TodoMangmentApplication.java
+++ src/main/java/com/usman/todo/TodoMangmentApplication.java
@@ -1,10 +1,17 @@
 package com.usman.todo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoMangmentApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoMangmentApplication.class, args);
