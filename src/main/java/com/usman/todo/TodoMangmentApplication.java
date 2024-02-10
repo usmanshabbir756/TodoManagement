1b04d2dac00b0c00f853ec801bb928ca639aa20e
usmanshabbir756 <usmanshabbir756@gmail.com>
2024-02-10 13:06:34 +0500

    intialize

---
+++ src/main/java/com/usman/todo/TodoMangmentApplication.java
@@ -0,0 +1,13 @@
+package com.usman.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoMangmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoMangmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usman/todo/TodoMangmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usman.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoMangmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
