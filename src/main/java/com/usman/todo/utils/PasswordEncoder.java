1edd73ae0a6e84eb3103c15b89c264b979348293
usmanshabbir756 <usmanshabbir756@gmail.com>
2024-03-13 15:18:32 +0500

    create to gent=erate encode password

---
+++ src/main/java/com/usman/todo/utils/PasswordEncoder.java
@@ -0,0 +1,11 @@
+package com.usman.todo.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoder {
+    public static void main(String[] args) {
+        org.springframework.security.crypto.password.PasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("usman"));
+        System.out.println(passwordEncoder.encode("admin"));
+    }
+}
